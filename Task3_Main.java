ce1dc6ec9c58399c77e2b19236b56958faea3fa0
serp11 <135606530+serp11@users.noreply.github.com>
2024-02-20 06:27:03 +0200

    Create Task3_Main.java

---
+++ Task3_Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        var api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(new MyBot());
+    }
+}
+//https://t.me/serp_task3_bot
